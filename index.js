c48bb6d1144486c183642d8f187fc08a6a326986
Jayson Sam Fong <fcr808investments@gmail.com>
2018-01-09 06:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = "74"
-var message = `${name} is less then "40" tall`
+var message = `${name} is ${height} "40" tall`
 
 module.exports = {name, height, message}
